b702a70dac36feeca3a23504ab16945ff39531a0
Admin <Admin@DESKTOP-KD802P4>
2023-04-20 04:15:59 +0530

    rest chnages

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      All right reserved &copy; Paras Mahajan Demo
+    </div>
+  );
+};
+
+export default Footer;
